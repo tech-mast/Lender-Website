df84c3c8393dd038ebf95c0a10308291339aec12
tech-mast <75795440+tech-mast@users.noreply.github.com>
2021-09-27 13:58:58 -0400

    Update DashboardSuccess.js
    set gear feature

--- lender_frontend/src/pages/DashboardSuccess.js
+++ lender_frontend/src/pages/DashboardSuccess.js
@@ -1,11 +1,12 @@
-import {Form, Button,Row,Col,Container} from 'react-bootstrap';
+import {Row,Col,Container} from 'react-bootstrap';
 import React, {useState, useEffect} from 'react';
-import {useHistory, useParams, Link} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import Dashboard from './Dashboard';
 import FourBadges from '../components/FourBadges';
 import Card from '../components/Card';
-import { ExampleChart, Pie3D, Column3D, Msline, Doughnut2D,MslineCredit } from '../components/Charts';
+import {Pie3D, Column3D, Msline, MslineCredit } from '../components/Charts';
 import styled from 'styled-components';
+import loadingImage from '../pictures/preloader.gif';
 
 function DashboardSuccess() {
 
@@ -20,24 +21,18 @@ function DashboardSuccess() {
     const[monthlyDescCreditChartData2, SetMonthlyDescCreditChartData2] = useState([]);
     const[pie3DInfoChartData, SetPie3DInfoChartData] = useState([]);
     const[cardBadgesInfoChartData, SetCardBadgesInfoChartData] = useState([]);
-    let{zumId,clientEmail} = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    let{zumId} = useParams();
     let user = JSON.parse(localStorage.getItem('user-info'));
-    //mock data
-    // user={
-    //     email:'steven@steven.com',
-    //     isMatch:'yes',
-    //     zumId:'110f9e90-cc88-4c16-9b34-81e437b8a9bc'
-    // }
-    //
     const[zumIdIndex, SetZumIdIndex] = useState(zumId? zumId:(user&&user.zumId));
-    const[dashboardName, SetDashboardName] = useState(clientEmail? clientEmail:(user&&user.email));
-    let monthlyDescData =[];
+
     let arrayHelper3 =[];
-    const[descData, SetDescData] = useState([]);
+    // const[descData, SetDescData] = useState([]);
 
     useEffect(async()=>{
-    
-        let item = {}
+        // set gear
+        setIsLoading(true)
+        let item = {Username: 'jstlouis+1@phinanse.com',Password:'Phinanse2021'}
         let tokenResult = await fetch("https://api-sandbox.zumrails.com/api/authorize",{
             method: 'POST',
             body: JSON.stringify(item),
@@ -45,12 +40,15 @@ function DashboardSuccess() {
                 "Content-Type" : "application/json",
                 "Accept" : 'application/json'
             }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Error:'+ error)
         });
         
         tokenResult = await tokenResult.json();
         let tokenRefresh = tokenResult.result.Token;
 
-        // let zumId = user.zumId
         let result = await fetch("https://api-sandbox.zumrails.com/api/user/getbankaccountinformation/" +zumIdIndex,{
             method: 'GET',
             // body: JSON.stringify(item),
@@ -59,18 +57,30 @@ function DashboardSuccess() {
                 "Accept" : 'application/json',
                 "Authorization" : "Bearer "+tokenRefresh
             }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('Error:'+ error)
         });
-        
         result = await result.json();
-        let n = result.result.Institution === "RBC Royal Bank" ? 1 : 0
-        let dataStr = result.result.Accounts[n].Transactions;
-        // console.log(dataStr);
 
+        
+        let m=0;
+        for(let i= 0; i<25;i++){
+            if(!result.result.Accounts[i].Transactions.length){          
+            }
+            else{
+                m=i;
+                break;
+            }
+            
+        }
+
+        let dataStr = result.result.Accounts[m].Transactions;
         const monthlyIncome = dataStr.reduce((total, item) => {
             const {Date, Credit} = item;
             if(!Date) return total;
             let dataStrShort = Date.substring(0,7);
-            // console.log("date" + dataStrShort);
 
             if( !total[dataStrShort]) {
             total[dataStrShort] = { label: dataStrShort, value: Credit};
@@ -137,32 +147,28 @@ function DashboardSuccess() {
         // statis monthly description debit data
         const monthlyDescDebit = dataStr.reduce((total, item) => {
             const {Date, Description, Debit} = item;
-            // console.log(Description)
             if(!Date) return total;
             let dataStrShort = Date.substring(0,7);
             if(!total[dataStrShort])
             {
                 total[dataStrShort] = {};
             }
-            // else{
 
-                if( !total[dataStrShort][Description]) {
-                    total[dataStrShort][Description] = { label: Description, value: Debit, date:dataStrShort};
-                } else {
-                    total[dataStrShort][Description] = {
-                        ...total[dataStrShort][Description],
-                        value: total[dataStrShort][Description].value +item.Debit,
-                        date: item.Date.substring(0,7)
-                    };
-                    
+            if( !total[dataStrShort][Description]) {
+                total[dataStrShort][Description] = { label: Description, value: Debit, date:dataStrShort};
+            } else {
+                total[dataStrShort][Description] = {
+                    ...total[dataStrShort][Description],
+                    value: total[dataStrShort][Description].value +item.Debit,
+                    date: item.Date.substring(0,7)
+                };
                 
-                }
-            // }            
+            
+            }
+           
             return total;
         },{});
         SetMonthlyDescDebitData(monthlyDescDebit);
-        // console.log(monthlyDescDebit);
-
 
         // set first Msline chart debit data
         let monthlyDescDebitChartDataHelp = [];
@@ -281,7 +287,7 @@ function DashboardSuccess() {
         SetMonthlyDescCreditChartData2(Object.values(totalCredit));
 
         // fetch card info data
-        console.log(result)
+
         let cardBadgesInfo={};
         cardBadgesInfo['userName']=result.result.HolderName;
         cardBadgesInfo['institution']=result.result.Institution.substring(0,4);
@@ -293,116 +299,64 @@ function DashboardSuccess() {
             sum= sum+item.Balance;
             return Math.ceil(sum);
         },0)
-        console.log(cardBadgesInfo);
+
         SetCardBadgesInfoChartData(cardBadgesInfo);
         //fetch Pie chart data
         let pie3DInfo={};
         result.result.Accounts.forEach(a=>{
             pie3DInfo[a.Title]={label:a.Title ? a.Title.substring(6,17):'No Description', value:a.Balance}
         })
-        // console.log(pie3DInfo);
         SetPie3DInfoChartData(Object.values(pie3DInfo));
-        // console.log(pie3DInfoChartData);
-
+        //gear finish
+        setIsLoading(false)
     },[])
 
     return(
         user ?
             user.isMatch === "yes" || zumId ?
-            <Wrapper>
-                <Container>
-                    <Row>
-                        <FourBadges data ={cardBadgesInfoChartData}/>
-                    </Row>
-                    {/* <WrapperSecondLine> */}
-                        <Row xs={1} md={2} style={{marginTop:'10px', marginBottom:'60px'}}>
-                            <Col md={{span:7,offset:0.5}} style={{marginTop:'30px'}}>
-                                <Card data ={cardBadgesInfoChartData}/>
-                            </Col>
-                            <Col md={{span:5,offset:0.5}} style={{marginTop:'30px'}}>
-                                <article>
-                                    <Pie3D style={{borderColor:'green solid 3px'}} data ={pie3DInfoChartData}/>
-
-                                </article>
-                            </Col>
+            isLoading?
+                <img src={loadingImage} className = 'loading-img' alt = 'loading'/>
+                :
+                <Wrapper>
+                    <Container>
+                        <Row>
+                            <FourBadges data ={cardBadgesInfoChartData}/>
                         </Row>
-                    {/* </WrapperSecondLine> */}
-                    <Row style={{marginTop:'60px', marginBottom:'60px'}}>
-                        <article>
-                            <Column3D data = {monthlyIncomeChartData}></Column3D>
-                        </article>
-                    </Row>
-                    <Row xs={1} md={2}style={{marginTop:'60px', marginBottom:'60px'}}>
-                            {/* <Col md={{span:6}}> */}
-                            <Col style={{marginTop:'50px'}}>
-                        <article>
-                                <Msline data = {monthlyDescDebitChartData1} data2={monthlyDescDebitChartData2}></Msline>
-                        </article>
-                            </Col>
-                            {/* <Col md={{span:6}}> */}
-                            <Col style={{marginTop:'50px'}}>
-                        <article>
-                                <MslineCredit data = {monthlyDescCreditChartData1} data2={monthlyDescCreditChartData2}></MslineCredit>
-                        </article>
-                            </Col>
-                    </Row>
-                    <Row style={{marginTop:'60px'}}>
-                    </Row>
-                
-                </Container>
-                {/* <h1>Monthly Income List</h1>
-                <div className = "col-sm-8 offset-sm-3 mt-5">
-                    <table>
-                        <tr>
-                            <td>Month</td>
-                            <td>Monthly Income</td>
-                        </tr>
-                        <tr> <br/></tr>
-                        {
-                            monthlyIncome.map((item) =>
-                            <tr>
-                                <td>{item.label}</td>
-                                <td>{item.value}</td>
-                            </tr>
-                            )
-                        }
-                    </table>
-                </div>
-                <div>
-                {                      
-                    Object.values(monthlyDescDebitData).map(item =>   
-                        <>                    
-                            <h6 className = "mt-5">Debit statistic month {Object.values(item)[0].date}</h6>
-                            <div className = "col-sm-8 offset-sm-4 mt-5 ml-3">
-                                {Object.values(item).map(itmeIn =>
-                                    <tr>
-                                    <td>{itmeIn.label}</td>
-                                    <td>{itmeIn.value}</td>
-                                    <td>{itmeIn.date}</td>
-                                    </tr>
-                                )}
-                            </div>
-                        </>
-                    )
-                }
-                </div>           
-                {
-                    arrayHelper2.map(item =>   
-                        <>                     
-                            <h6 className = "mt-5">Credit statistic month {item[0].date}</h6>
-                            <div className = "col-sm-8 offset-sm-4 mt-5 ml-3">
-                                {item.map(itmeIn =>
-                                    <tr>
-                                    <td>{itmeIn.label}</td>
-                                    <td>{itmeIn.value}</td>
-                                    <td>{itmeIn.date}</td>
-                                    </tr>
-                                )}
-                            </div>
-                        </>
-                    )
-                }            */}
-            </Wrapper>
+
+                            <Row xs={1} md={2} style={{marginTop:'10px', marginBottom:'60px'}}>
+                                <Col md={{span:7,offset:0.5}} style={{marginTop:'30px'}}>
+                                    <Card data ={cardBadgesInfoChartData}/>
+                                </Col>
+                                <Col md={{span:5,offset:0.5}} style={{marginTop:'30px'}}>
+                                    <article>
+                                        <Pie3D style={{borderColor:'green solid 3px'}} data ={pie3DInfoChartData}/>
+
+                                    </article>
+                                </Col>
+                            </Row>
+        
+                        <Row style={{marginTop:'60px', marginBottom:'60px'}}>
+                            <article>
+                                <Column3D data = {monthlyIncomeChartData}></Column3D>
+                            </article>
+                        </Row>
+                        <Row xs={1} md={2}style={{marginTop:'60px', marginBottom:'60px'}}>
+                                <Col style={{marginTop:'50px'}}>
+                            <article>
+                                    <Msline data = {monthlyDescDebitChartData1} data2={monthlyDescDebitChartData2}></Msline>
+                            </article>
+                                </Col>
+                                <Col style={{marginTop:'50px'}}>
+                            <article>
+                                    <MslineCredit data = {monthlyDescCreditChartData1} data2={monthlyDescCreditChartData2}></MslineCredit>
+                            </article>
+                                </Col>
+                        </Row>
+                        <Row style={{marginTop:'60px'}}>
+                        </Row>
+                    
+                    </Container>
+                </Wrapper>
             :
             <>
                 <Dashboard />
